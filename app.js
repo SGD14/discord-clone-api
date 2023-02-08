68c0f5e2e5936aa942c151051c07eb5601e3d7e7
Dani <24252795+SGD14@users.noreply.github.com>
2023-02-08 11:36:53 +0100

    Rename friends router to friendRequest

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const authRouter = require("./src/routes/auth");
-const friendsRouter = require("./src/routes/friends");
+const friendRequestRouter = require("./src/routes/friendRequest");
 
 // Init Mongoose
 mongoose.connect(process.env.DATABASE_URI);
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRouter);
-app.use("/friends", friendsRouter);
+app.use("/friend-requests", friendRequestRouter);
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
